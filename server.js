0a3f8e4d2e8efb2d63ad5074efc628245367b8b0
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-05-21 15:27:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
-require('dotenv').config();
-
-const userRoutes = require('./routes/userRoutes');
-const cowRoutes = require('./routes/cowRoutes');
-
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/cows', cowRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
