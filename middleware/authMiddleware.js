3c8b41c2406529c0ef6a967e0861d8b4c92dc956
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-05-21 15:14:15 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: 'Access denied' });
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch {
+  } catch (err) {
     res.status(400).json({ error: 'Invalid token' });
   }
 };
