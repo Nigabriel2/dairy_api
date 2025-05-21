e7c158c901796a532cb6150fcb15d46b1d65dca0
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-05-21 14:26:39 +0200

    Update cowController.js

--- controllers/cowController.js
+++ controllers/cowController.js
@@ -1,4 +1,4 @@
-const { insertCow, fetchCows } = require('../models/cowModel');
+const { insertCow, fetchCows, updateCow } = require('../models/cowModel');
 
 exports.addCow = async (req, res) => {
   const { tag_number, name, breed, date_of_birth, purchase_date } = req.body;
@@ -14,6 +14,21 @@ exports.getCows = async (req, res) => {
   try {
     const cows = await fetchCows(req.user.id);
     res.json(cows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.editCow = async (req, res) => {
+  const { tag_number, name, breed, date_of_birth, purchase_date } = req.body;
+  const cowId = req.params.id;
+
+  try {
+    const cow = await updateCow(cowId, tag_number, name, breed, date_of_birth, purchase_date, req.user.id);
+    if (!cow) {
+      return res.status(404).json({ error: 'Cow not found or not owned by user' });
+    }
+    res.json(cow);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
