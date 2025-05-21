21c06383314f11a03a78bc685da429725a4cff66
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-05-21 14:27:18 +0200

    Update cowRoutes.js

--- routes/cowRoutes.js
+++ routes/cowRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addCow, getCows } = require('../controllers/cowController');
+const { addCow, getCows, editCow } = require('../controllers/cowController');
 const auth = require('../middleware/authMiddleware');
 
 router.post('/', auth, addCow);
 router.get('/', auth, getCows);
+router.put('/:id', auth, editCow); // 🆕 PUT method for editing cow
 
 module.exports = router;
