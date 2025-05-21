b3978a71ff03c690c357529da4f5be49e0efbf2d
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-05-21 14:27:53 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  getProfile,
+} = require('../controllers/userController');
+const auth = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/all', getAllUsers);
+router.get('/me', auth, getProfile); // 🔐 protected route
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
