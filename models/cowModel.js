066c602469d581baaf1af588bf25bfb2dfab5308
niyomugabo gabriel <157947610+Nigabriel2@users.noreply.github.com>
2025-06-10 22:58:51 +0200

    Update cowModel.js

--- models/cowModel.js
+++ models/cowModel.js
@@ -1,21 +1,22 @@
-    const db = require('../config/db');
+const db = require('../config/db');
 
-    exports.insertCow = async (tag, name, breed, dob, purchase, userId) => {
-      const result = await db.query(
-        `INSERT INTO cows (tag_number, name, breed, date_of_birth, purchase_date, user_id)
-        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-        [tag, name, breed, dob, purchase, userId]
-      );
-      return result.rows[0];
-    };
+exports.insertCow = async (tag, name, breed, dob, purchase, userId) => {
+  const result = await db.query(
+    `INSERT INTO cows (tag_number, name, breed, date_of_birth, purchase_date, user_id)
+     VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+    [tag, name, breed, dob, purchase, userId]
+  );
+  return result.rows[0];
+};
 
-    exports.fetchCows = async (userId) => {
-      const result = await db.query(
-        'SELECT * FROM cows WHERE user_id = $1',
-        [userId]
-      );
-      return result.rows;
-    };
+exports.fetchCows = async (userId) => {
+  const result = await db.query(
+    'SELECT * FROM cows WHERE user_id = $1',
+    [userId]
+  );
+  return result.rows;
+};
+
 exports.updateCow = async (id, tag, name, breed, dob, purchase, userId) => {
   const result = await db.query(
     `UPDATE cows
@@ -23,6 +24,16 @@ exports.updateCow = async (id, tag, name, breed, dob, purchase, userId) => {
      WHERE id = $6 AND user_id = $7
      RETURNING *`,
     [tag, name, breed, dob, purchase, id, userId]
+  );
+  return result.rows[0];
+};
+
+exports.deleteCow = async (id, userId) => {
+  const result = await db.query(
+    `DELETE FROM cows
+     WHERE id = $1 AND user_id = $2
+     RETURNING *`,
+    [id, userId]
   );
   return result.rows[0];
 };
